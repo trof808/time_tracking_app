034fbd07572a8c895b94ceed8e0a2adaac329de2
trof <reatrof@gmail.com>
2017-07-18 22:17:21 +0300

    project structure

---
+++ src/containers/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Root extends Component {
+    render() {
+        return (
+            <div>
+                hi
+            </div>
+        )
+    }
+}
+
+export default Root;
